3bbca30b65fc6a0427d6f8197bf5e8b60a691c43
gojoego <josephjgallego@gmail.com>
2026-01-29 23:38:43 -0600

    updating purpose, how-to and about me

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,16 +1,50 @@
 export default function AboutPage(){
     return (
-        <main className="max-w-3xl mx-auto py-12 px-4 space-y-6">
-            <h1 className="text-3xl font-bold tracking-tight">
-                about the moriah project 
-            </h1>
-            <p className="text-muted-foreground text-lg">
-                placeholder text 
-            </p>
-            <p>
-                placeholder text 
-            </p>
+        <main className="max-w-3xl mx-auto py-12 px-4 space-y-12">
+            <header className="space-y-2 text-center">
+                <h1 className="text-3xl font-semibold">
+                    About The Moriah Project 
+                </h1>
+            </header>
+            <section aria-labelledby="about-me">
+                <h2 id="about-me" className="text-xl font-medium">
+                    About Me
+                </h2>
+                <p className="mt-3 text-sm leading-relaxed">
+                    My name is Joe. Born in the sun of San Diego and raised in the fog of San Francisco, I currently live in the
+                    heat of the Texan sun. I am social worker who found himself immersed in the burgeoning tech scene of 
+                    Silicon Valley. I eventually learned to code and decided to start building applications that could possibly 
+                    help individuals with mental health struggles. As someone who has struggled with clinical depression since 
+                    age 5, I strive to build products imbued with empathy, kindness and acceptance.  
+                </p>
+            </section>
 
+            <section aria-labelledby="about-site">
+                <h2 id="about-site" className="text-xl font-medium">
+                    About This Website
+                </h2>
+                <div className="mt-3 space-y-4 text-sm leading-relaxed text-muted-foreground">
+                    <p>
+                            At the end of 2022, my friend Moriah passed away from suicide. We were not particularly close but her 
+                            death had an incredible impact on me. After I moved cities, we lost touch. There were times I thought 
+                            about texting her. It was usually after some intense workout because we had been workout buddies. But I never did. 
+                    </p>
+                    <p>
+                        And I have never stopped wondering if I there was something I could have done to prevent her passing. Maybe, just maybe 
+                        if I had reached out to her, maybe that one text would have made a difference. What could I have said? What could I 
+                        have told her?             
+                    </p>
+                    <p>
+                        My hope is that someone might be able to find answers to these questions in this forum. I built this space so 
+                        that those in mourning could see that there are others experiencing a similar pain.
+                    </p>
+                    <p>
+                        If you are here because you have lost someone, you have my sincere condolences. I hope this space helps in your 
+                        grieving process and that we can all heal together.
+                    </p>                    
+                </div>
+
+            </section>
         </main>
     )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,13 +10,27 @@ export default function HomePage() {
     return (
         <main className="mx-auto max-w-3xl space-y-8 px-4">
             <header className="space-y-2 text-center">
-                <h1 className="text-3xl font-semibold">
-                    welcome to the Moriah Project
+                <h1 className="text-5xl font-semibold">
+                    Welcome to the Moriah Project 
                 </h1>
-                <p className="text-muted-foreground text-sm">
-                    a space to honor those who have passed 
+                <p className="text-muted-foreground text-2xl">
+                    here you can express what you wish you could say to someone who has passed away from suicide  
                 </p>
             </header>
+            <section>
+                <h2 className="text-2xl font-medium text-center">How This Works...</h2>
+                <ol className="mt-3 space-y-2 text-muted-foreground text-xl list-decimal list-inside">
+                    <li>
+                        provide some details about the individual - give a little context - provide as much info as you want  
+                    </li>
+                    <li>
+                        tell us what you wish you could say to this individual 
+                    </li>
+                    <Link href="/posts/1" className="mt-3 inline-block text-lg font-medium underline-offset-4 hover:underline">
+                        here is the example: the first post
+                    </Link>
+                </ol>
+            </section>
             <section>
                 <div className="flex items-center justify-between mb-4">
                     <h2 className="text-xl font-semibold tracking-tight">
