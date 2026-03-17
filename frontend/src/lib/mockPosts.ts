0a19a4933d67d0b2ee5d58dc3672f7732e658af9
gojoego <josephjgallego@gmail.com>
2026-03-17 14:50:20 -0500

    adjusted fields on mock data

--- frontend/src/lib/mockPosts.ts
+++ frontend/src/lib/mockPosts.ts
@@ -3,58 +3,74 @@ import type { Post } from "@/types/post";
 export const mockPosts: Post[] = [
     {
         id: "1",
-        deceasedName: "Moriah",
+        deceased_name: "Moriah",
         background: "how I knew Moriah",
         content: "how we lost Moriah to suicide",
-        createdAt: "2026-01-04",
+        created_at: "2026-01-04",
+        status: "",
+        author_name: ""
     },
     {
         id: "2",
-        deceasedName: "Thomas",
+        deceased_name: "Thomas",
         background: "how I met Thomas",
         content: "how Thomas was taken from us",
-        createdAt: "2026-01-03",
+        created_at: "2026-01-03",
+        status: "",
+        author_name: ""
     },
     {
         id: "3",
-        deceasedName: "Danny",
+        deceased_name: "Danny",
         background: "how I met Danny",
         content: "how Danny was taken from us",
-        createdAt: "2021-01-09",
+        created_at: "2021-01-09",
+        status: "",
+        author_name: ""
     },
     {
         id: "4",
-        deceasedName: "uncle",
+        deceased_name: "uncle",
         background: "how my uncle influenced me",
         content: "how my uncle was taken from us",
-        createdAt: "2022-03-11",
+        created_at: "2022-03-11",
+        status: "",
+        author_name: ""
     },
     {
         id: "5",
-        deceasedName: "father",
+        deceased_name: "father",
         background: "how I knew my father",
         content: "how father was taken from us",
-        createdAt: "2023-02-22",
+        created_at: "2023-02-22",
+        status: "",
+        author_name: ""
     },
     {
         id: "6",
-        deceasedName: "auntie",
+        deceased_name: "auntie",
         background: "how I knew my auntie",
         content: "how my auntie was taken from us",
-        createdAt: "2022-01-03",
+        created_at: "2022-01-03",
+        status: "",
+        author_name: ""
     },
     {
         id: "7",
-        deceasedName: "grandpa",
+        deceased_name: "grandpa",
         background: "how my grandpa influenced me",
         content: "how my grandpa was taken from us",
-        createdAt: "2024-01-01",
+        created_at: "2024-01-01",
+        status: "",
+        author_name: ""
     },
     {
         id: "8",
-        deceasedName: "eight",
+        deceased_name: "eight",
         background: "eight",
         content: "eight",
-        createdAt: "2025-01-03",
+        created_at: "2025-01-03",
+        status: "",
+        author_name: ""
     },
 ]
