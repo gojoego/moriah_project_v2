3f70699b1359a111c108326cff13453d59b227cb
gojoego <josephjgallego@gmail.com>
2026-04-20 20:33:07 -0700

    mock auth middleware and add success + unauthorized tests for /users/me

--- backend/src/tests/users.me.test.ts
+++ backend/src/tests/users.me.test.ts
@@ -1,23 +1,52 @@
 import request from "supertest";
 import app from "../app";
 import { getUserById } from "../db/queries/users";
-import {  } from "../db/queries/users";
+import { authMiddleware } from "../middleware/auth";
+import { error } from "node:console";
 
 jest.mock("../db/queries/users", () => ({
-  getUserById: jest.fn(),
+  	getUserById: jest.fn(),
+}));
+
+jest.mock("../middleware/auth", () => ({
+  	authMiddleware: jest.fn(),
 }));
 
 describe("GET /api/users/me", () => {
-  it("returns the current user", async () => {
-    (getUserById as jest.Mock).mockResolvedValue({
-      id: "123",
-      display_name: "Joe",
-      email: "joe@test.com",
-    });
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
 
-    const res = await request(app).get("/api/users/me");
+	it("returns the current user (200)", async () => {
+		(authMiddleware as jest.Mock).mockImplementation(
+			(req: any, _res: any, next: any) => {
+				req.user = {id: "123", email: "joe@test.com" };
+				next();
+			}
+		);
 
-    expect(res.status).toBe(200);
-    expect(res.body.email).toBeDefined();
-  });
+		(getUserById as jest.Mock).mockResolvedValue({
+			id: "123",
+			display_name: "Joe",
+			email: "joe@test.com",
+		});
+
+		const res = await request(app).get("/api/users/me");
+
+		expect(res.status).toBe(200);
+		expect(res.body.email).toBeDefined();
+	});
+
+	it("returns 401 if no auth user (unauthorized)", async () => {
+		(authMiddleware as jest.Mock).mockImplementation(
+			(_req: any, res: any) => {
+				return res.status(401).json({ error: "Unauthorized"});
+			}	
+		);
+
+		const res = await request(app).get("api/users/me");
+
+		expect(res.status).toBe(401);
+		expect(res.body.error).toBeDefined();
+	});
 });
