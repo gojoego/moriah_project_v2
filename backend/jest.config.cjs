096bface238b505b8b7f0dfd9738f2db076c4ed2
gojoego <josephjgallego@gmail.com>
2026-08-09 15:37:44 -0500

    separating prod tsconfig.json into prod and test configs

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,5 +1,13 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/tests/**/*.test.ts"],
+    transform: {
+        "^.+\\.ts$": [
+            "ts-jest",
+            {
+                tsconfig: "tsconfig.test.json",
+            },
+        ],
+    },
 };
